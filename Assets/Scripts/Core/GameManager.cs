b5386834723a1dab31a0fc4df4fad0f1177c63f0
abuska <abigelcsabai@gmail.com>
2022-11-12 15:32:53 +0100

    feat: Add main menu

---
+++ Assets/Scripts/Core/GameManager.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]public GameObject completeLevelUI;
+    [SerializeField]public GameObject completeAllLevelUI;
+    [SerializeField]public GameObject gameOverUI;
+
+    public bool gameHasEnded = false;
+    public float restartDelay = 1f;
+
+    private float mainMenuTimer = Mathf.Infinity;
+    private float mainMenuTime = 1f;
+    private bool isPause;
+
+    private void Awake(){
+        Time.timeScale = 1;
+        isPause = false;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.Escape) && SceneManager.GetActiveScene().name != "MainMenu" && mainMenuTimer > mainMenuTime ){
+            if(!isPause){
+                PauseGame();
+            }
+        }
+        mainMenuTimer +=Time.deltaTime;
+    }
+
+    public void PauseGame(){
+        Time.timeScale = 0;
+        SceneManager.LoadScene("GameMenu", LoadSceneMode.Additive);
+        mainMenuTimer = 0;
+        isPause = true;
+    }
+
+    public void ContinueGame(){
+        SceneManager.UnloadSceneAsync("GameMenu");
+        Time.timeScale = 1;
+        mainMenuTimer = 0;
+        isPause = false;
+    }
+
+    public void Restart(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void CompleteLevel(){
+        if(SceneManager.sceneCountInBuildSettings-1==SceneManager.GetActiveScene().buildIndex){
+            completeAllLevelUI.SetActive(true);
+        }else{
+            completeLevelUI.SetActive(true);
+        }
+      
+    }
+
+    public void GameOver (){
+        if(gameHasEnded == false){
+            gameHasEnded = true;
+            gameOverUI.SetActive(true);
+        }
+   
+    }
+    public void goToMainMenu(){
+         SceneManager.LoadScene("MainMenu");
+    }
+
+
+}
---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{ 
+    private GameManager gameManager;
+
+    private void Awake(){
+        gameManager = FindObjectOfType<GameManager>();
+    }
+    public void StartGame(){
+        Debug.Log("doin something");
+        SceneManager.LoadScene("Level_1");
+    }
+    public void ContinueGame(){
+        gameManager.ContinueGame();
+    }
+    public void RestartLevel(){
+        gameManager.Restart();
+    }
+    public void QuitGame(){
+        Application.Quit();
+    }
+    public void goToMainManu(){
+        SceneManager.LoadScene("MainMenu");
+    }
+}
