4a6f7eee5e0b497617a8bdbe8ef6895bb506d772
PeterToth <petertortel@hotmail.com>
2022-11-13 17:45:18 +0100

    feature: add activation tile

--- Assets/Scripts/OpenDoor.cs
+++ Assets/Scripts/OpenDoor.cs
@@ -6,31 +6,59 @@ using UnityEngine;
 public class OpenDoor : MonoBehaviour
 {
     public GameObject DoorToOpen;
+
     // Start is called before the first frame update
+    public float MoveSpeed = 4.0f;
+
+    public bool TriggeredOnce = false;
+
+    public Animator anim;
+
+    public float deactivationTime;
+
+    private float deactivationTimer = Mathf.Infinity;
+
+    public bool IsActive = false;
+
+    private bool FirstActivationDone = false;
+
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (
+            ((TriggeredOnce && !FirstActivationDone) || !TriggeredOnce) &&
+            deactivationTimer <= 0 &&
+            IsActive
+        )
+        {
+            anim.SetTrigger("IsDeactivated");
+            deactivationTimer = Mathf.Infinity;
+            IsActive = false;
+        }
+        if (deactivationTimer >= 0)
+        {
+            deactivationTimer -= Time.deltaTime;
+        }
     }
 
     private void OnTriggerStay(Collider other)
-    {        
-        foreach(KeyCode kcode in Enum.GetValues(typeof(KeyCode)))
+    {
+        if (
+            !IsActive &&
+            ((TriggeredOnce && !FirstActivationDone) || !TriggeredOnce)
+        )
         {
-            if (Input.GetKey(kcode)){
-                if(kcode == KeyCode.E){
-                    Destroy(DoorToOpen);
-                }
+            if (Input.GetKey(KeyCode.E))
+            {
+                anim.SetTrigger("IsActivated");
+                deactivationTimer = deactivationTime;
+                IsActive = true;
+                FirstActivationDone = true;
             }
         }
-
-
-    
-        
     }
 }
