237f20f34d56a863cd8ee7f4c93a4130dbc87680
PeterToth <petertortel@hotmail.com>
2022-11-28 18:55:21 +0100

    feat:  add keys
    level and ui

--- Assets/KeyItem.cs
+++ Assets/KeyItem.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class KeyItem : MonoBehaviour
 {
     public List<GameObject> LocksToOpen;
+
+    public string keyType = "Blue";
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +26,29 @@ public class KeyItem : MonoBehaviour
             foreach (var Lock in LocksToOpen)
             {
                 Lock.GetComponent<OpenDoor>().SetKeyCollected(true);
+            }
+            switch (keyType)
+            {
+                case "Blue":
+                    GameObject
+                        .Find("Key_Blue_UI")
+                        .GetComponent<Image>()
+                        .enabled = true;
+                    break;
+                case "Yellow":
+                    GameObject
+                        .Find("Key_Yellow_UI")
+                        .GetComponent<Image>()
+                        .enabled = true;
+                    break;
+                case "Red":
+                    GameObject
+                        .Find("Key_Red_UI")
+                        .GetComponent<Image>()
+                        .enabled = true;
+                    break;
+                default:
+                    break;
             }
 
             Destroy (gameObject);
